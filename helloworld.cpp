8e50dc842f2a9ff75032d9caa9b903a4c0605114
Abhishek Basu <iabhishekbasu@gmail.com>
2021-10-31 22:33:17 +0530

    hellow world

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Declaration of header
+using namespace std; // Using std namespace
+
+int main() // Function main through which program execution begins
+{
+  cout << "Hello World"; // Displaying "hello world"
+
+  return 0; // Returning 0 indicates success to the operating system
+}
