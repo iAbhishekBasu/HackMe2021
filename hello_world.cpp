6c37f3ec7ed8a87fa278a69cd814361b76d30955
Restyled.io <commits@restyled.io>
2021-10-31 17:05:29 +0000

    Restyled by astyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@ using namespace std; // Using std namespace
 
 int main() // Function main through which program execution begins
 {
-  cout << "Hello World"; // Displaying "hello world"
+    cout << "Hello World"; // Displaying "hello world"
 
-  return 0; // Returning 0 indicates success to the operating system
+    return 0; // Returning 0 indicates success to the operating system
 }
